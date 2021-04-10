d96d40ee346073948f15a4467452c5f4bc236fec
Raquel <raquelmolinareche@gmail.com>
2021-04-11 01:05:15 +0200

    programando server y client básico

--- Practica2/rpc/calculadora_server.c
+++ Practica2/rpc/calculadora_server.c
@@ -127,11 +127,13 @@ sumavectores_1_svc(vectorData v1, vectorData v2,  struct svc_req *rqstp)
 	//Se redimensiona
 	values = malloc(v1.vectorData_len*sizeof(double));
 
+	printf("4: hola\n");
+
 	//Se calcula el resultado de la operacion
 	for(int i = 0; i < v1.vectorData_len;i++){
 
-		values->vectorData_val[i]  =  v1.vectorData_val[i] + v2.vectorData_val[i];
-		printf("%f + %f = %f\n", v1.vectorData_val[i],v2.vectorData_val[i],values->vectorData_val[i]);
+		result.responseVectores_u.v.vectorData_val[i]  =  v1.vectorData_val[i] + v2.vectorData_val[i];
+		printf("%f + %f = %f\n", v1.vectorData_val[i],v2.vectorData_val[i],result.responseVectores_u.v.vectorData_val[i]);
 	}
 
 	printf("4: hola\n");
